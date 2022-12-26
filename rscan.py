bb7ee3ffb52343d01cc44daf6be49de8e8b8e2ad
h0tcat <h0tcat@MacBook-Air.local>
2022-12-26 22:11:47 +0900

    add customize

--- rscan.py
+++ rscan.py
@@ -1,86 +1,56 @@
-#!/usr/bin/python
-
-
-#################################################################################
-#                                                                               #
-#.______               _______.  ______     ___      .__   __.                  #
-#|   _  \             /       | /      |   /   \     |  \ |  |                  # 
-#|  |_)  |    ______ |   (----`|  ,----'  /  ^  \    |   \|  |                  #
-#|      /    |______| \   \    |  |      /  /_\  \   |  . `  |                  #
-#|  |\  \----.    .----)   |   |  `----./  _____  \  |  |\   |                  #
-#| _| `._____|    |_______/     \______/__/     \__\ |__| \__|  v0.1.2          #
-#                                                                               #
-#GNU PL - 2015 - ca333                                                          # 
-#                                                                               #         
-#USE AT OWN RISK!                                                               #
-#################################################################################
-
 import json
-import urllib2
+import requests
 import time
 import sys
 
-#for some reason blockchain.info api-chain is 59711 blocks short..
 blockstart = 170399
 blockstart += 59711
-blockcount = urllib2.urlopen("https://blockchain.info/de/q/getblockcount").read()
 
-print "WELCOME TO R-scan v0.1.2!"
+print("WELCOME TO R-scan Python3 Edition v0.1.2!")
 
-print "ADDRESS-R-SCAN: "
-addr = raw_input("type address:  ")
-urladdr = "https://blockchain.info/de/rawaddr/" + str(addr)
-#control api-url
-print urladdr 
-addrdata = json.load(urllib2.urlopen(urladdr))
-print "Data for pubkey: " + str(addr)
-print "number of txs: " + str(addrdata['n_tx'])
-#tx-details:
-y = 0
-inputs = []
-while y < addrdata['n_tx']:	
-	print "#################################################################################"
-	print "TX nr :" + str(y+1)
-	print "hash: " + str(addrdata['txs'][y]['hash'])
-	print "number of inputs: " + str(addrdata['txs'][y]['vin_sz'])
-	#only if 
-	#if addrdata['txs'][y]['vin_sz'] > 1:
-	zy = 0
-	while zy < addrdata['txs'][y]['vin_sz']:
-		print "Input-ScriptNR " + str(zy+1) + " :" + str(addrdata['txs'][y]['inputs'][zy]['script'])
-		inputs.append(addrdata['txs'][y]['inputs'][zy]['script'])
-		zy += 1
+addr_files = input("address list file path --> ")
+addr_files = open(addr_files, "r")
+tx_n = 0
+while(True):
+	found_reused_r = False
+	addr = addr_files.readline()
+	if not addr:
+		break
+	addr = "1EHNa6Q4Jz2uvNExL497mE43ikXhwF6kZm"
+	urladdr = f"https://sochain.com/api/v2/get_tx_spent/BTC/{str(addr)}"
+	headers = {"content-type": "application/json"}
+
+	tx_information= requests.get(urladdr).json()["data"]["txs"]
+	tx_id = tx_information	
+	tx_count = len(tx_information)
+	tx_n = 0
+	x = 1
 	
-	y += 1
-	
-print "compare: "
+	try:
+		while not found_reused_r:
+			urladdr = f"https://sochain.com/api/v2/tx/BTC/{tx_id[tx_n]['txid']}"
+			rawdata_res = requests.get(urladdr, headers = headers).json()
+			rawdata = rawdata_res["data"]["inputs"][0]["script_hex"]
+			prev_r = rawdata[10:74]
+			while not found_reused_r: 
+				urladdr = f"https://sochain.com/api/v2/tx/BTC/{tx_id[tx_n + x]['txid']}"
+				next_rawdata = requests.get(urladdr).json()["data"]["inputs"][0]["script_hex"]
+				print(f"compare : {prev_r} <=> {next_rawdata[10:74]}")
+				if prev_r == next_rawdata[10:74]:
+					print('\a')
+					print("-----------------------------")
+					print(f"Address : {addr}")
+					print(f"TXID : {tx_id[tx_n]['txid']}")
+					print(f"Reused R TXID : {tx_id[tx_n + x]['txid']}")
+					print("Resued R-Value: ")
+					print(next_rawdata[10:74])
+					print("-----------------------------")
+					found_reused_r = True
+				prev_r = next_rawdata[10:74]
+					
+				x += 1
+			tx_n += 1
+	except Exception:
+		pass
 
-xi = 0
-zi = 1
-lenx = len(inputs)
-alert = 0
-
-#compare the sig values in each input script
-while xi < lenx-1:
-	x = 0
-	while x < lenx-zi: 
-		if inputs[xi][10:74] == inputs[x+zi][10:74]:
-			print "In Input NR: " + str(xi) + "[global increment] " + str(inputs[xi])
-			print('\a')
-                        print "Resued R-Value: "
-			print inputs[x+zi][10:74]
-                        alert += 1
-
-		x += 1
-		
-	zi += 1
-	xi += 1
-
-#check duplicates
-#alert when everything ok
-
-if alert < 1:
-	print "Good pubKey. No problems."
-
-
-sys.exit()
+addr_files.close()
